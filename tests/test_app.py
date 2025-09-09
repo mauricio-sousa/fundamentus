538e2e3bb1c318884140add4beef864cde738973
Mauricio Sousa Pereira <mspereira@uolinc.com>
2025-09-08 22:40:14 -0300

    refactor: adicionar mocks nos testes para simular dados da função get_data

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,45 @@
+from unittest.mock import patch
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
+
+@patch("main.get_data")
+def test_get_main(mock_get_data):
+    mock_get_data.return_value = {"ABEV3": {"Cotacao": 15.0}}
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json()
 
 
-def test_get_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json()
+@patch("main.get_data")
+def test_get_ticker(mock_get_data):
+    mock_get_data.return_value = {"ABEV3": {"Cotacao": 15.0}}
+    with TestClient(app) as client:
+        response = client.get("/ticker/ABEV3")
+        assert response.status_code == 200
+        assert response.json() == {"Cotacao": 15.0}
 
 
-def test_get_ticker():
-    response = client.get("/ticker/abev3")
-    assert response.status_code == 200
-    assert response.json()
+@patch("main.get_data")
+def test_get_tickers(mock_get_data):
+    mock_get_data.return_value = {
+        "ABEV3": {"Cotacao": 15.0},
+        "VALE3": {"Cotacao": 70.0},
+    }
+    with TestClient(app) as client:
+        response = client.get("/tickers")
+        assert response.status_code == 200
+        assert response.json() == {
+            "ABEV3": {"Cotacao": 15.0},
+            "VALE3": {"Cotacao": 70.0},
+        }
 
 
-def test_get_tickers():
-    response = client.get("/tickers")
-    assert response.status_code == 200
-    assert response.json()
-
-
-def test_get_ticker_not_found():
-    response = client.get("/ticker/abev")
-    assert response.status_code == 404
-    assert response.json()
+@patch("main.get_data")
+def test_get_ticker_not_found(mock_get_data):
+    mock_get_data.return_value = {"ABEV3": {"Cotacao": 15.0}}
+    with TestClient(app) as client:
+        response = client.get("/ticker/abev")
+        assert response.status_code == 404
+        assert response.json()["detail"] == "Ticker: ABEV não encontrado!"
